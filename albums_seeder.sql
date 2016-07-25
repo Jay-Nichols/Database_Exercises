987745b46ea9b6877e0a991b40c1bd7d77f8314c
jay nichols <jay_nichols_3@hotmail.com>
2016-07-25 16:43:52 -0500

    added data to table

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Nirvana', 'Nevermind', 1991, 16.7, 'Grunge'),
+ ('Michael Jackson', 'Thriller', 1982, 45.4, 'Pop'),
+ ('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard Rock'), 
+ ('Eagles', 'Their Greatest Hits', 1976, 32.2, 'Rock'),
+ ('Pink Floyd', 'Dark Side of the Moon', 1973, 22.7, 'Progressive Rock'),
+ ('The Beatles', 'Sgt. Pepper', 1967, 13.1, 'Rock');
